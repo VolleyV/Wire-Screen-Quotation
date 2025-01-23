cf17afeaa6dd548fe26cb005bc9ee8dbc65910ff
VolleyV <nopphoom@gmail.com>
2025-01-23 17:42:41 +0700

    fix deploy

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,23 @@
 "use client";
+
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function HomePage() {
+function SearchParamsComponent() {
   const searchParams = useSearchParams();
   const name = searchParams.get("name");
 
+  return <p>ชื่อ: {name}</p>;
+}
+
+export default function HomePage() {
   return (
     <div>
       <h1>Welcome to the Home Page!</h1>
-      <p>ชื่อ: {name}</p>
+      <Suspense fallback={<p>Loading...</p>}>
+        <SearchParamsComponent />
+      </Suspense>
       <div>
         <Link href="/" className="text-3xl font-bold underline">
           Go Back (Link Component)
